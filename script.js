e57becae6e9b5b3b9de86f354e4d097b2cf59bf8
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-08-07 13:05:37 +0700

    Color palette changed

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ $(function () {
     $(document).scroll(function () {
         var $header = $("header");
         var $logo = $("#header-logo");
+        var $nav = $("header a")
 
         console.log("hey");
 
         $header.toggleClass('scrolled', $(this).scrollTop() > $header.height());
         $logo.toggleClass('scrolled', $(this).scrollTop() > $header.height());
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height());
     });
 });
