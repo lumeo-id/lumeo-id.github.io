f66f82bdfaa35276fa5e73a8f4e87ecbc28f0fe3
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-08-08 17:43:55 +0700

    Burger navbar

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 $(function () {
     $(document).scroll(function () {
-        var $header = $("header");
-        var $logo = $("#header-logo");
-        var $nav = $("header a")
+        var $nav = $("nav");
+        var $navLogo = $("#nav-logo");
+        var $navLinks = $("nav a");
+        var $burger = $(".burger div");
 
-        console.log("hey");
-
-        $header.toggleClass('scrolled', $(this).scrollTop() > $header.height());
-        $logo.toggleClass('scrolled', $(this).scrollTop() > $header.height());
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height());
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $navLogo.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $navLinks.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $burger.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const burgerBar = document.querySelector(".burger div");
+    const nav = document.querySelector("nav");
+    const navLogo = document.querySelector("#nav-logo");
+    const navLinks = document.querySelector('.nav-links');
+    const navList = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active');
+        nav.classList.toggle('scrolled');
+        burgerBar.classList.toggle('scrolled');
+        navLogo.classList.toggle('scrolled');
+    });
+
+    navList.forEach((link, index) => {
+        link.style.animation = 'navLinkFade 0.5s ease forwards 1s';
+    });
+}
+
+const app = () => {
+    navSlide();
+}
+
+app();
