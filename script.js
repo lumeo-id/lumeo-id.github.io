69250e60b828b1e1b53646ae68eaf5e5e112144e
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-09-02 14:35:22 +0700

    Penawaran section

--- script.js
+++ script.js
@@ -39,6 +39,44 @@ const navSlide = () => {
     });
 }
 
+function startTimer() {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    // "Sep 30, 2021 00:00:00"
+    let targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 2);
+
+    let countDown = new Date(targetDate).getTime(),
+        x = setInterval(function() {
+            let now = new Date().getTime();
+            let distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+            let headline = document.getElementById("headline"),
+                countdown = document.getElementById("countdown"),
+                content = document.getElementById("content");
+
+            headline.innerText = "It's my targetDate!";
+            countdown.style.display = "none";
+            content.style.display = "block";
+
+            clearInterval(x);
+        }
+        //seconds
+    }, 0)
+}
+
+startTimer();
+
 const app = () => {
     navSlide();
 }
